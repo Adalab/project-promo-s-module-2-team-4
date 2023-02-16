c07cf7ecd7aa15b17af60ec0339955c0cf70855e
marinacr92 <marinacaro92@gmail.com>
2023-02-16 17:43:05 +0100

    Remove duplicated js

--- src/js/share.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function handleClickItems(event) {
-  event.preventDefault();
-  showItems(divTwitter);
-  buttonShare.classList.add('button--share__off');
-}
-
-buttonShare.addEventListener('click', handleClickItems);
